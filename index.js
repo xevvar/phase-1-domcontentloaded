a325c96c51eb33faf0bf6de2d1444c032f8f4c04
Anthony <gachinigaithuma@gmail.com>
2023-11-07 15:56:01 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+
+    newContent();
+    
+
+})
+function newContent(){
+    const text = document.getElementById('text');
+    text.textContent = 'This is really cool!';
+    
+
+}
